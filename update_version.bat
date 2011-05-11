4e96dba78feefe405c98622eae23174fb9d0f07d
Jan E <jeebjp@gmail.com>
2011-05-11 17:15:41 +0300

    (CCCP Patch) Set version numbers and VSFilter's CCCP resources.

--- update_version.bat
+++ update_version.bat
@@ -18,7 +18,11 @@ ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
 ECHO You should (re)install TortoiseSVN.
 ECHO I'll use MPC_VERSION_REV=0 for now.
 
-ECHO #define MPC_VERSION_REV 0 > "include\Version_rev.h"
+ECHO #define MPC_VERSION_REV 3103       >  "include\Version_rev.h"
+ECHO #define ACTUAL_BUILD_YEAR 2011     >> "include\Version_rev.h"
+ECHO #define ACTUAL_BUILD_MONTH 5       >> "include\Version_rev.h"
+ECHO #define ACTUAL_BUILD_DAY 11        >> "include\Version_rev.h"
+ECHO #define ACTUAL_BUILD_TIME 17:12:00 >> "include\Version_rev.h"
 COPY /Y /V "src\apps\mplayerc\res\mpc-hc.exe.manifest.template" "src\apps\mplayerc\res\mpc-hc.exe.manifest" >NUL
 
 ENDLOCAL
